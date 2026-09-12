a42582c19113c8515068bec9a2fae2893532e1ab
Ayanmsheikh <ayanm.sheikh@icloud.com>
2026-09-11 21:26:44 -0400

    updated inline comments as per Dr.Fu's last grade

--- Lab 2/Blink Test/src/main.cpp
+++ Lab 2/Blink Test/src/main.cpp
@@ -4,8 +4,8 @@
 #define LED_PIN 13
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  Serial.begin(115200); // Start the Serial connection at 115200 baud
+  pinMode(LED_PIN, OUTPUT); // ams926 Set the LED pin as an output
+  Serial.begin(115200); // ams926 Start the Serial connection at 115200 baud
   // TODO: Define your baud rates
   
 }
@@ -13,12 +13,12 @@ void setup() {
 void loop() {
   // TODO: Define your text output
   // Serial.println("_____"); // Print the words to the Serial Monitor.
-  Serial.println("HELLO WORLD");  
+  Serial.println("HELLO WORLD");   // ams926 Print the words to the Serial Monitor.
   
   // TODO: Set up your delay in milliseconds
-  digitalWrite(LED_PIN, HIGH); // LED On
-  delay(1000);
-  digitalWrite(LED_PIN, LOW); // LED Off
-  delay(1000);
+  digitalWrite(LED_PIN, HIGH); // ams926 LED On
+  delay(1000); // ams926 Wait for 1 second
+  digitalWrite(LED_PIN, LOW); // ams926 LED Off
+  delay(1000); // ams926 Wait for 1 second
   
 }
